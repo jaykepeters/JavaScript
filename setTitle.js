8bfe3dab7e81acf25e0d25b312c951c2da1efe9f
Jayke Peters <jaykepeters@gmail.com>
2019-05-20 15:45:09 -0500

    Create setTitle.js

---
+++ setTitle.js
@@ -0,0 +1,34 @@
+function getCurrentPage() {
+    var sPath = window.location.pathname;
+    var sPage = sPath.substring(sPath.lastIndexOf('/') + 1);
+    return sPage;
+}
+
+function getPageTitleFromHeading() {
+    var headings = ['h1', 'h2', 'h3'];
+    var firstHeading;
+    var titleText = '';
+
+    for (i = 0; i < headings.length; i++) {
+        heading = headings[i];
+        firstHeading = document.getElementsByTagName(heading)[0];
+        if (firstHeading != undefined) {
+            titleText = firstHeading.innerHTML;
+            return titleText;
+        }
+    }
+}
+
+function setDocumentTitle() {
+    var currentTitle = document.title;
+    var assumedTitle = getPageTitleFromHeading();
+    if (currentTitle == assumedTitle) {
+        document.title += ' | Home'
+    } else {
+        if (getCurrentPage != undefined) {
+            document.title += ' | '+assumedTitle;
+        }
+    }
+}
+
+setDocumentTitle();
